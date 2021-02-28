532631876dc35362a8ad290d8eddd95b2d4a0414
Ken <kchau@microsoft.com>
2021-02-28 15:38:03 -0800

    adding colors

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        product: {
+          access: '#a4373a',
+          exchange: '#0078d4',
+          excel: '#217346',
+          office: '#d83b01',
+          onedrive: '#0078d4',
+          onenote: '#7719aa',
+          planner: '#31752f',
+          powerapps: '#742774',
+          powerpoint: '#b7472a',
+          publisher: '#077568',
+          sharepoint: '#0078d4',
+          skype: '#0078d4',
+          sway: '#008272',
+          teams: '#6264a7',
+          visio: '#3955a3',
+          word: '#2b579a',
+          yammer: '#106ebe'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
